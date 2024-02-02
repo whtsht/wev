4c1e1c67ad37ab8963a5fa09d8cad446c4efbba0
whtsht <whiteshirt0079@gmail.com>
2024-02-02 20:17:05 +0900

    feat: stylesheet in html

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,33 +1,47 @@
 use combine::Parser;
 use std::io::Result;
-use wev::{css, html, style::to_styled_node};
+use wev::{css, dom::Node, html, style::to_styled_node};
 
 fn main() -> Result<()> {
-    let dom = html::nodes()
+    let node = html::nodes()
         .parse(
             r#"
-                <div>
-                    <p foo="bar">hello world</p>
-                </div>
-                   "#,
+<body>
+  <div class="none"><p>this should not be shown</p></div>
+  <style>
+    .none {
+      display: none;
+    }
+    .inline {
+      display: inline;
+    }
+  </style>
+</body>"#,
         )
         .unwrap()
         .0;
-    let stylesheet = css::stylesheet(
-        r#"
-            div {
-                color:red;
-            }
-            p {
-                color:blue;
-            }
-            p[foo=bar] {
-                color:yellow;
-            }
-            "#
-        .into(),
-    );
-    let nodes = to_styled_node(&dom[0], &stylesheet);
+
+    let root_node = Box::new(Node {
+        node_type: wev::dom::NodeType::Element(wev::dom::Element {
+            tag_name: "".into(),
+            attributes: vec![].into_iter().collect(),
+        }),
+        children: node,
+    });
+
+    let style_tag = wev::cssom::SimpleSelector::TypeSelector {
+        tag_name: "style".into(),
+    };
+    let css = wev::dom::select(&root_node, &style_tag);
+
+    let css = css
+        .get(0)
+        .and_then(|n| n.children.get(0))
+        .and_then(|style| style.to_text())
+        .unwrap_or_default();
+
+    let stylesheet = css::stylesheet(&css);
+    let nodes = to_styled_node(&root_node, &stylesheet);
 
     wev::start(nodes.as_ref().unwrap())
 }
--- src/dom.rs
+++ src/dom.rs
@@ -1,11 +1,30 @@
 use std::collections::HashMap;
 
+use crate::cssom::Selector;
+
 pub type AttrMap = HashMap<String, String>;
 
 #[derive(Debug, PartialEq)]
 pub struct Node {
     pub node_type: NodeType,
     pub children: Vec<Box<Node>>,
+}
+
+impl Node {
+    pub fn to_text(&self) -> Option<String> {
+        match &self.node_type {
+            NodeType::Element { .. } => None,
+            NodeType::Text(Text { data }) => Some(data.clone()),
+        }
+    }
+}
+
+pub fn select<'a>(node: &'a Node, selector: &'a Selector) -> Vec<&'a Box<Node>> {
+    node.children
+        .iter()
+        .filter(|&n| selector.matches(n))
+        .chain(node.children.iter().flat_map(|n| select(n, selector)))
+        .collect()
 }
 
 #[derive(Debug, PartialEq)]
